ed974aba0836b0a2e1e0525de11005de58d1e18b
tarunkumar456 <tk8657270@gmail.com>
2025-03-18 21:11:13 +0530

    second commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,110 +2,75 @@ const catchasyncerror = require("../middleware/catchasyncerror");
 const userModels = require("../models/userModels");
 const ErrorHandler = require('../utils/errorhandler');
 const sendtoken = require('../utils/jwttoken');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
+// Register User (Student or Teacher)
 exports.registerUser = catchasyncerror(async (req, resp) => {
-    const { name, email, password } = req.body;
-    const user = await userModels.create({
-        name, email, password
-    })
+    const { name, email, password, role } = req.body;
+
+    // Validate role
+    if (!["teacher", "student"].includes(role)) {
+        return resp.status(400).json({
+            success: false,
+            message: "Invalid role. Role must be either 'teacher' or 'student'."
+        });
+    }
+
+    const user = await userModels.create({ name, email, password, role });
     sendtoken(user, 201, resp);
-})
-//login user
+});
+
+// Login User
 exports.loginUser = catchasyncerror(async (req, resp, next) => {
     const { email, password } = req.body;
-    //check if both present
+
     if (!email || !password) {
-        return next(new ErrorHandler("Please Enter Email & Password", 400));
+        return next(new ErrorHandler("Please enter Email & Password", 400));
     }
 
     const user = await userModels.findOne({ email }).select("+password");
 
     if (!user) {
-        return next(new ErrorHandler("Invlaid Email & Password", 401))
+        return next(new ErrorHandler("Invalid Email or Password", 401));
     }
+
     const isPasswordMatched = await user.comparepassword(password);
 
     if (!isPasswordMatched) {
-        return next(new ErrorHandler("Invlaid Email & Password", 401))
+        return next(new ErrorHandler("Invalid Email or Password", 401));
     }
 
     sendtoken(user, 200, resp);
-})
+});
 
-// //logout user
+// Logout User
 exports.logout = catchasyncerror((req, resp, next) => {
-
     resp.cookie("token", null, {
         expires: new Date(Date.now()),
         httpOnly: true,
-        secure:true,
+        secure: true,
         sameSite: 'None'
-    })
+    });
+
     resp.status(200).json({
         success: true,
         message: "Logged Out"
-    })
-})
+    });
+});
 
-//get data
-// exports.getData = catchasyncerror(async (req, resp, next) => {
-//     let user = await userModels.find({ _id: req.user._id });
-//     if (!user) return next(new ErrorHandler("User not found", 404))
-//     let quiz = user[0]
-
-//     resp.status(200).json({
-//         success: true,
-//         quiz
-//     });
-// })
-
-
-// //add data
-// exports.addData = catchasyncerror(async (req, resp, next) => {
-//     const newquiz = {
-//         marks: req.body.marks,
-//         correct: req.body.correct,
-//         accuracy: req.body.accuracy,
-//         averagetime: req.body.avg
-//     }
-//     const user = await userModels.find({ _id: req.user._id });
-//     if (!user) return next(new ErrorHandler("User not found", 404))
-//     user[0].quiz.push(newquiz)
-//     await user[0].save({ validateBeforeSave: false });
-//     resp.status(200).json({
-//         success: true,
-//         user
-//     });
-// })
-
-
-
-// //authenticated
+// Check if Authenticated
 exports.isAuth = catchasyncerror(async (req, resp, next) => {
     const { token } = req.cookies;
+
     if (!token) {
-        return next(new ErrorHandler("please login to access it", 401));
+        return next(new ErrorHandler("Please login to access this resource", 401));
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     req.user = await userModels.findById(decoded.id);
+
     resp.status(200).json({
-        success: true
+        success: true,
+        role: req.user.role
     });
-
-
-})
-
-// //get all data
-// exports.data = catchasyncerror(async (req, resp, next) => {
-//     let user = await userModels.find();
-//     if (!user) return next(new ErrorHandler("User not found", 404))
-//     let quiz = user
-
-//     resp.status(200).json({
-//         success: true,
-//         quiz
-//     });
-// })
+});
